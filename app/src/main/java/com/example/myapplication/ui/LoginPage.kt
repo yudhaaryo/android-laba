11a3a8069c1b24a4e5b3484cfe7b931d37adf3d3
yudhaaryo1234 <yudhaaryo1234@gmail.com>
2023-09-06 15:58:45 +0700

    init

---
+++ app/src/main/java/com/example/myapplication/ui/LoginPage.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class LoginPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_page)
+    }
+}
